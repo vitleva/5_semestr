d7577c3d94e13ab071423caaacb03761b406beb9
Анастасия Витлева <anvitleva@gmail.com>
2025-09-05 21:49:42 +0700

    Тройные кавычки

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "<!DOCTYPE html>" \
-        "<html>" \
-        "   <body>" \
-        "       <h1>web-сервер на flask</h1>" \
-        "   </body>" \
-        "</html>"
+    return """<!DOCTYPE html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+           </body>
+        </html>"""
 
 

a92f8357ef255bf618c40daaaaba7f00ae4e9395
Анастасия Витлева <anvitleva@gmail.com>
2025-09-05 21:58:18 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,12 +2,29 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!DOCTYPE html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
+               <a href="/author">author</a>
            </body>
         </html>"""
 
+@app.route("/author")
+def author():
+    name = "Витлева Анастасия Александровна"
+    group = "ФБИ-31"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
 

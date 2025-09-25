352982a35635375f3e5eca0a9302653f913ce8f4
Анастасия Витлева <anvitleva@gmail.com>
2025-09-25 09:09:22 +0700

    Имя студента

--- app.py
+++ app.py
@@ -381,4 +381,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Витлева Анастасия'
+    return render_template('example.html', name=name)

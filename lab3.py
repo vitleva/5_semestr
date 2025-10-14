b6c91e081ab76cae0d84a879dcc391327d87478d
Анастасия Витлева <anvitleva@gmail.com>
2025-10-14 14:47:00 +0700

    Аноним

--- lab3.py
+++ lab3.py
@@ -3,9 +3,10 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3')
 def lab():
-    name = request.cookies.get('name')
+    name = request.cookies.get('name') or 'Аноним'
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    age = request.cookies.get('age') or 'Неизвестный'
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 @lab3.route('/lab3/cookie')
 def cookie():

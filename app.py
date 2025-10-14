0d3db9f60498b1b96c933a8bb51d744f8b2b37b7
Анастасия Витлева <anvitleva@gmail.com>
2025-10-14 08:54:22 +0700

    Заготовка к третьей лабораторной

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 import datetime
 app = Flask(__name__)
-app.register_blueprint(lab1, lab2)
+app.register_blueprint(lab1)
+app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 # список логов
 access_log = []
 
@@ -135,7 +138,8 @@ def index():
         </header>
         <nav>
             <a href="/lab1">Первая лабораторная</a><br>
-            <a href="/lab2">Вторая лабораторная</a>
+            <a href="/lab2">Вторая лабораторная</a><br>
+            <a href="/lab3">Третья лабораторная</a>
         </nav>
         <footer>
             Витлева Анастасия Александровна, ФБИ-31, 3 курс, 2025

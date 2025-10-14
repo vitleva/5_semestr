1f1ca3f5f56d4aac962548889404b056f0e29cf3
Анастасия Витлева <anvitleva@gmail.com>
2025-10-14 14:10:22 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -62,3 +62,34 @@ def pay():
 def success():
     price = request.args.get('price')
     return render_template('lab3/success.html', price=price)
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    font_family = request.args.get('font_family')
+    
+    if any([color, bg_color, font_size, font_family]):
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if bg_color:
+            resp.set_cookie('bg_color', bg_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if font_family:
+            resp.set_cookie('font_family', font_family)
+        return resp
+    
+    color = request.cookies.get('color')
+    bg_color = request.cookies.get('bg_color')
+    font_size = request.cookies.get('font_size')
+    font_family = request.cookies.get('font_family')
+    
+    resp = make_response(render_template('lab3/settings.html', 
+                                       color=color, 
+                                       bg_color=bg_color, 
+                                       font_size=font_size, 
+                                       font_family=font_family))
+    return resp

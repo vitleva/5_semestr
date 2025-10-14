1ddf8ca817634eb52c8e8b170c72c88d541d20e1
Анастасия Витлева <anvitleva@gmail.com>
2025-10-14 11:09:57 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -33,5 +33,32 @@ def form1():
     age = request.args.get('age')
     if age == '':
         errors['age'] = 'Заполните поле!'
+
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user=user,age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    return render_template('lab3/pay.html', price=price, drink=drink)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price')
+    return render_template('lab3/success.html', price=price)

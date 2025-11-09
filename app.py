5223b6df829693bcb2fc06686aca72916c3d4b24
Анастасия Витлева <anvitleva@gmail.com>
2025-11-09 23:52:24 +0700

    правки

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, request, redirect, abort, render_template, os
+from flask import Flask, url_for, request, redirect, abort, render_template
+import os
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3

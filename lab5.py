c42a74738f440f6c6e6f75c241ad25299e19e456
Анастасия Витлева <anvitleva@gmail.com>
2025-11-09 22:35:17 +0700

    Исправление SQL-инъекций

--- lab5.py
+++ lab5.py
@@ -37,13 +37,13 @@ def register():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
@@ -60,7 +60,7 @@ def login():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -94,8 +94,8 @@ def create():
     cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                VALUES ({user_id}, '{title}', '{article_text}');")
+    cur.execute("INSERT INTO articles(user_id, title, article_text) \
+                VALUES (%s, %s, %s);", (user_id, title, article_text))
     
     db_close(conn, cur)
     return redirect('/lab5')

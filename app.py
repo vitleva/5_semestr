bfa91d38f17c005ea855b0bc0c837dd32855abec
Анастасия Витлева <anvitleva@gmail.com>
2025-11-26 16:59:58 +0700

    Заготовка к 7-ой лабораторнойЦ

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
@@ -16,6 +17,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 # список логов
 access_log = []
 
@@ -152,6 +154,7 @@ def index():
             <a href="/lab4">Четвертая лабораторная</a><br>
             <a href="/lab5">Пятая лабораторная</a><br>
             <a href="/lab6">Шестая лабораторная</a><br>
+            <a href="/lab7">Седьмая лабораторная</a><br>
         </nav>
         <footer>
             Витлева Анастасия Александровна, ФБИ-31, 3 курс, 2025

77a66bc5efb21e003f95b67ee4287641c382797e
Анастасия Витлева <anvitleva@gmail.com>
2025-12-10 18:48:25 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, current_app
+from flask import Blueprint, session, render_template, request, redirect, current_app
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
@@ -36,3 +37,34 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8')
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/login.html',
+                               error='Имя пользователя не должно быть пустым')
+
+    if not password_form:
+        return render_template('lab8/login.html',
+                               error='Пароль не должен быть пустым')
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8')
+    
+    return render_template('/lab8/login.html', 
+                           error = 'Ошибка входа: логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return 'список статей'

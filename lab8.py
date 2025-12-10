18bf5f0d958f4b9d6f891b6e47e1b90856e000a5
Анастасия Витлева <anvitleva@gmail.com>
2025-12-10 18:51:30 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, session, render_template, request, redirect, current_app
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
@@ -68,3 +68,9 @@ def login():
 @login_required
 def article_list():
     return 'список статей'
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8')

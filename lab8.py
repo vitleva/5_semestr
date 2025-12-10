915be61a6b223dfc587a726884978feaf2a27ce8
Анастасия Витлева <anvitleva@gmail.com>
2025-12-10 18:17:43 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,13 +1,38 @@
-import os
-from flask import Blueprint, render_template, request, abort, current_app
-from datetime import datetime
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import sqlite3
-from os import path
+from flask import Blueprint, render_template, request, redirect, current_app
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8')
 def lab():
     return render_template('lab8/lab8.html')
+
+@lab8.route('/lab8/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html',
+                               error='Имя пользователя не должно быть пустым')
+
+    if not password_form:
+        return render_template('lab8/register.html',
+                               error='Пароль не должен быть пустым')
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                            error = 'Такой пользователь уже существует')
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8')

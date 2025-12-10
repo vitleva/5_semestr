2c392c4bcd15968e4c1a008761c5b1a7cbad39de
Анастасия Витлева <anvitleva@gmail.com>
2025-12-10 17:42:19 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, url_for, request, redirect, abort, render_template
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -13,6 +16,31 @@ import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+db_name = 'anastasia_vitleva_orm'
+db_user = 'anastasia_vitkeva_orm'
+db_password = '123'
+host_ip = '127.0.0.1'
+host_port = 5432
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'anastasia_vitleva_orm'
+    db_user = 'anastasia_vitleva_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'anastasia_vitleva_orm')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
